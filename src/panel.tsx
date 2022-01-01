f05422598fd79b4450526c275faf41380c2ddfde
Volcomix <Volcomix@users.noreply.github.com>
2022-01-01 20:47:16 +0100

    Bootstrap rendering in devtools

--- src/panel.tsx
+++ src/panel.tsx
@@ -1,5 +1,5 @@
 import { h, render } from 'preact'
-import { useEffect } from 'preact/hooks'
+import { useEffect, useState } from 'preact/hooks'
 import useActiveTab from './hooks/useActiveTab'
 import useDetectionStatus from './hooks/useDetectionStatus'
 import './panel.css'
@@ -8,20 +8,32 @@ import { DetectionMessage } from './types'
 function Panel() {
   const activeTab = useActiveTab()
   const detectionStatus = useDetectionStatus(activeTab)
+  const [ctx, setCtx] = useState<CanvasRenderingContext2D>()
 
   const video = detectionStatus?.video
 
-  function handleMessage(message: DetectionMessage) {}
+  function handleMessage(message: DetectionMessage) {
+    if (!ctx || !video) {
+      return
+    }
+    if (message.type !== 'Poses') {
+      return
+    }
+    ctx.clearRect(0, 0, video.width, video.height)
+    ctx.fillStyle = 'red'
+    ctx.fillRect(0, 0, video.width, video.height)
+  }
 
   useEffect(() => {
     chrome.runtime.onMessage.addListener(handleMessage)
     return () => chrome.runtime.onMessage.removeListener(handleMessage)
-  }, [])
+  }, [ctx, video])
 
   return (
     <div className="Panel">
       <canvas
         className="Panel-canvas"
+        ref={(canvas) => canvas && setCtx(canvas.getContext('2d')!)}
         width={video?.width}
         height={video?.height}
       />
