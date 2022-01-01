f266a2d5f2890df4346b169cff4be49ee74718f2
Volcomix <Volcomix@users.noreply.github.com>
2022-01-01 17:29:07 +0100

    Make video select controlled

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,4 +1,5 @@
 import { render } from 'preact'
+import { useEffect, useState } from 'preact/hooks'
 import useActiveTab from './hooks/useActiveTab'
 import useDetectionStatus from './hooks/useDetectionStatus'
 import useVideos from './hooks/useVideos'
@@ -10,6 +11,19 @@ function Popup() {
   const activeTab = useActiveTab()
   const detectionStatus = useDetectionStatus(activeTab)
   const videos = useVideos(activeTab, detectionStatus)
+  const [selectedVideo, setSelectedVideo] = useState(0)
+
+  useEffect(() => {
+    if (!videos?.length) {
+      return
+    }
+    let candidateVideos = videos.filter((video) => video.playing)
+    if (candidateVideos.length === 0) {
+      candidateVideos = [...videos]
+    }
+    candidateVideos.sort((a, b) => b.width - a.width)
+    setSelectedVideo(videos.indexOf(candidateVideos[0]))
+  }, [videos])
 
   if (!videos) {
     return <progress />
@@ -17,12 +31,6 @@ function Popup() {
   if (videos.length === 0) {
     return <span className="Popup-noVideos">No videos found</span>
   }
-
-  let candidateVideos = videos.filter((video) => video.playing)
-  if (candidateVideos.length === 0) {
-    candidateVideos = [...videos]
-  }
-  candidateVideos.sort((a, b) => b.width - a.width)
 
   function handleDetectionButtonClick() {
     if (!activeTab) {
@@ -42,9 +50,13 @@ function Popup() {
       <select
         className="Popup-videos"
         disabled={detectionStatus !== undefined && detectionStatus !== 'loaded'}
+        value={selectedVideo}
+        onInput={(event) =>
+          setSelectedVideo(Number((event.target as HTMLSelectElement).value))
+        }
       >
         {videos.map((video, i) => (
-          <option key={i} value={i} selected={video === candidateVideos[0]}>
+          <option key={i} value={i}>
             {video.playing && '▸ '}
             {video.src.substr(0, videoSrcMaxLength) || 'video'}
             {video.src.length > videoSrcMaxLength && '…'}
